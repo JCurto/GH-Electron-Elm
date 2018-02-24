8d56fd3438edb60e77a0f1631ef468255f00c3e3
Jeffery Curto <JCurto@users.noreply.github.com>
2018-02-24 15:12:52 -0500

    Reversing String Input Test

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,50 @@
-import Html
+module Main exposing (..)
 
-main : Html.Html msg
-main = Html.text "hello world"
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { content : String }
+
+
+model : Model
+model =
+    { content = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change newContent ->
+            { model | content = newContent }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ placeholder "Text to reverse", onInput Change ] []
+        , div [] [ text (String.reverse model.content) ]
+        ]
