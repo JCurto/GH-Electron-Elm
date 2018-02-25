4ed6c200863c6e8f2ca70e072148fee5f8236980
Jeffery Curto <JCurto@users.noreply.github.com>
2018-02-25 15:16:48 -0500

    Trying Out Damage Buttons

--- src/Main.elm
+++ src/Main.elm
@@ -24,26 +24,28 @@ type alias MonsterGroup =
     , monsters : List Monster
     }
 
-newMonsterGroup : String -> Int -> MonsterGroup
-newMonsterGroup class n =
+newMonsterGroup : String -> Int -> Int -> MonsterGroup
+newMonsterGroup class n health =
     { monsterClass = class
-    , monsters = newMonsterList n
+    , monsters = newMonsterList class n health
     }
 
 type alias Monster =
-    { monsterId : Int
+    { monsterClass : String
+    , monsterId : Int
     , maxHealth : Int
     , damage : Int
     }
 
-newMonsterList : Int -> List Monster
-newMonsterList n =
-    (List.map newMonster <| List.range 1 n)
+newMonsterList : String -> Int -> Int -> List Monster
+newMonsterList class n health =
+    (List.map (newMonster class health) (List.range 1 n))
 
-newMonster : Int -> Monster
-newMonster n =
-    { monsterId = n
-    , maxHealth = 10
+newMonster : String -> Int -> Int -> Monster
+newMonster class health monsterId =
+    { monsterClass = class
+    , monsterId = monsterId
+    , maxHealth = health
     , damage = 0
     }
 
@@ -51,8 +53,8 @@ newMonster n =
 model : Model
 model =
     { monsterGroups =
-        [ { monsterClass = "bandit", monsters = [ { monsterId = 1, maxHealth = 10, damage = 5 } ] }
-        , { monsterClass = "ooze", monsters = [ { monsterId = 1, maxHealth = 10, damage = 5 }, { monsterId = 1, maxHealth = 10, damage = 5 } ] }
+        [ { monsterClass = "bandit", monsters = [ { monsterClass = "bandit", monsterId = 1, maxHealth = 10, damage = 5 } ] }
+        , { monsterClass = "ooze", monsters = [ { monsterClass = "ooze", monsterId = 1, maxHealth = 10, damage = 5 }, { monsterClass = "ooze", monsterId = 2, maxHealth = 10, damage = 5 } ] }
         ]
     }
 
@@ -63,6 +65,8 @@ model =
 
 type Msg
     = AddMonster
+    | IncrementDamage Monster
+    | DecrementDamage Monster
 
 
 
@@ -73,7 +77,41 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         AddMonster ->
-            { model | monsterGroups = model.monsterGroups ++ [ newMonsterGroup "MonsterB" 4] }
+            { model | monsterGroups = model.monsterGroups ++ [ newMonsterGroup "MonsterB" 4 7] }
+
+        IncrementDamage existingMonster ->
+            let
+                updateMonsterGroup monsterGroup =
+                    if monsterGroup.monsterClass == existingMonster.monsterClass then
+                        { monsterGroup | monsters = List.map updateMonster monsterGroup.monsters }
+                    else
+                        monsterGroup
+
+                updateMonster monster =
+                    if monster.monsterId == existingMonster.monsterId then
+                        { monster | damage = monster.damage + 1 }
+                    else
+                        monster
+
+            in
+                { model | monsterGroups = List.map updateMonsterGroup model.monsterGroups }
+
+        DecrementDamage existingMonster ->
+            let
+                updateMonsterGroup monsterGroup =
+                    if monsterGroup.monsterClass == existingMonster.monsterClass then
+                        { monsterGroup | monsters = List.map updateMonster monsterGroup.monsters }
+                    else
+                        monsterGroup
+
+                updateMonster monster =
+                    if monster.monsterId == existingMonster.monsterId then
+                        { monster | damage = monster.damage - 1 }
+                    else
+                        monster
+
+            in
+                { model | monsterGroups = List.map updateMonsterGroup model.monsterGroups }
 
 
 
@@ -95,4 +133,8 @@ monsterGroup monsterGroup =
 
 monster : Monster -> Html Msg
 monster monster =
-    div [ class "monster" ] []
+    div [ class "monster" ]
+        [ text ((toString monster.damage) ++ "/" ++ (toString monster.maxHealth))
+        , button [ onClick (IncrementDamage monster) ] [ text "+" ]
+        , button [ onClick (DecrementDamage monster) ] [ text "-"]
+        ]
